5b633f83df7ccb11c099505661e2d8d0e5cbab9c
Sims Smith <simssmith1@gmail.com>
2018-12-26 11:48:58 -0600

    Sparker VR version

--- Accurate Vital Display.lua
+++ Accurate Vital Display.lua
@@ -17,6 +17,9 @@ dataref("xp_elevation", "sim/flightmodel/position/elevation")
 dataref("xp_indicated_airspeed", "sim/flightmodel/position/indicated_airspeed")
 dataref("xp_vh_ind_fpm2", "sim/flightmodel/position/vh_ind_fpm2")
 
+avd_lastClickX = 640 / 2
+avd_lastClickY = 480 / 2
+
 local show_AVD 	= true
 local left_x 	= 300
 local x_inc 	= 75
@@ -57,6 +60,57 @@ function print_Vital()
 	end
 	draw_string_Times_Roman_24(left_x + vital_inc, label_y + (label_y_inc * 1), ez_xp_vh_ind_fpm2)
 
+end
+
+function avd_on_draw(avd_wnd, avd_x, avd_y)
+	-- color (defined by it's RGB values)
+	glColor3f(1,1,1)
+	draw_string_Times_Roman_24(avd_x, avd_y + (label_y_inc * 4), "HDG")
+	ez_xp_psi = string.format("%03.0f",xp_psi);
+	draw_string_Times_Roman_24(avd_x + vital_inc, avd_y + (label_y_inc * 4), ez_xp_psi)
+
+	draw_string_Times_Roman_24(avd_x, avd_y + (label_y_inc * 2), "ALT")
+	ez_xp_elevation = string.format("%05.0f",xp_elevation * 3.28084)
+	draw_string_Times_Roman_24(avd_x + vital_inc, avd_y + (label_y_inc * 2), ez_xp_elevation)
+
+	draw_string_Times_Roman_24(avd_x, avd_y + (label_y_inc * 3), "SPD")
+	ez_xp_indicated_airspeed = string.format("%03.0f",xp_indicated_airspeed);
+	draw_string_Times_Roman_24(avd_x + vital_inc, avd_y + (label_y_inc * 3), ez_xp_indicated_airspeed)
+	
+	draw_string_Times_Roman_24(avd_x, avd_y + (label_y_inc * 1), "VVI")
+	ez_xp_vh_ind_fpm2 = string.format("%03.0f",xp_vh_ind_fpm2);
+	if ez_xp_vh_ind_fpm2 == "-0" then
+		ez_xp_vh_ind_fpm2 = "0";
+	end
+	draw_string_Times_Roman_24(avd_x + vital_inc, avd_y + (label_y_inc * 1), ez_xp_vh_ind_fpm2)
+
+        graphics.set_color(1, 1, 1, 1)
+	graphics.set_width(2)
+
+	graphics.draw_line(avd_x, avd_y, avd_x + x_inc, avd_y)
+	graphics.draw_line(avd_x, avd_y + y_inc, avd_x + x_inc, avd_y + y_inc)
+
+	graphics.draw_line(avd_x, avd_y, avd_x, avd_y + y_inc)
+	graphics.draw_line(avd_x + x_inc, avd_y, avd_x + x_inc, avd_y + y_inc)
+
+        -- Trying to get the toggle button to work but not yet.
+        if (avd_lastClickX > avd_x and avd_lastClickX < avd_x + x_inc) then
+		if (avd_lastClickY > avd_y and avd_lastClickY < avd_y + y_inc) then
+			AVD_Toggle()
+		end
+	end
+
+	if (show_AVD == true) then
+		glColor3f(1,1,1)
+		draw_string_Times_Roman_24(avd_x + 5, avd_y + 4, "HIDE")
+		glColor3f(0,0,0)
+		draw_string_Times_Roman_24(avd_x + 7, avd_y + 6, "HIDE")
+	else	
+		glColor3f(0,0,0)
+		draw_string_Times_Roman_24(avd_x + 1, avd_y + 2, "SHOW")
+		glColor3f(255,128,0)
+		draw_string_Times_Roman_24(avd_x + 3, avd_y + 4, "SHOW")
+	end
 end
 
 function display_action_button()
@@ -101,5 +155,25 @@ function vital_refresh()
 	display_action_button()
 end
 
+-- x and y are relative from the origin of the window, i.e. the lower left
+function avd_on_click(avd_wnd, avd_x, avd_y)
+    avd_lastClickX = avd_x
+    avd_lastClickY = avd_y
+end
+
+-- When on_close it called, it is illegal to do anything with the wnd variable
+-- It is also not allowed to create new windows in on_close!
+function avd_on_close(avd_wnd)
+end
+
+-- width, height, decoration style as per XPLMCreateWindowEx. 1 for solid background, 3 for transparent
+avd_wnd = float_wnd_create(200, 240, 1)
+float_wnd_set_title(avd_wnd, "Accurate Vital Display")
+float_wnd_set_ondraw(avd_wnd, "avd_on_draw")
+float_wnd_set_onclick(avd_wnd, "avd_on_click")
+float_wnd_set_onclose(avd_wnd, "avd_on_close")
+
 do_on_mouse_click("toogle_display()")
 do_every_draw("vital_refresh()")
+
+
